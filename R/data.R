c4f37e7aeb76bae36bcb89a58eb8812d81ba20d8
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-08-21 19:25:15 -0400

    typo

--- R/data.R
+++ R/data.R
@@ -57,7 +57,7 @@
 #' A table describing the Getty's progress in digitizing their sales catalogs.
 #' @format A data frame with 8393 ovservations of 4 variables:
 #' \describe{
-#'  \item{sale_begin}{POSICxt. The start date for the sale described by the catalog.}
+#'  \item{sale_begin}{POSIXct. The start date for the sale described by the catalog.}
 #'  \item{sale_no}{Character. The sale catalog id number.}
 #'  \item{is_input}{Logical. Has the catalog been input into the database yet?}
 #'  \item{year}{Integer. Year derived from \code{sale_begin}}
