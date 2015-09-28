8748b67bc9543d483ca24cd2bac05fdc0c931a9e
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-09-28 15:19:23 -0400

    Plotting helper functions
    Saves article plots as PNG and PDF in inst/plots.

---
+++ R/plot.R
@@ -0,0 +1,17 @@
+#' pngplot
+pngplot <- function(p, name, h = 5, w = 6) {
+  ggsave(p, filename = paste(name, "png", sep = "."), height = h, width = w)
+}
+
+#' pdfplot
+pdfplot <- function(p, name, h = 5, w = 6) {
+  ggsave(p, filename = paste(name, "pdf", sep = "."), height = h, width = w)
+}
+
+#' dualplot
+dualplot <- function(p, name, h = 5, w = 6) {
+  name <- paste("inst/plots", name, sep = "/")
+  pngplot(p, name, h, w)
+  pdfplot(p, name, h, w)
+  return(p)
+}
